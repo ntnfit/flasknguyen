53fd92cb248f6202c5cefc9b258ccb9b751a139f
Nguyễn Thanh Nguyên <58658850+ntnfit98@users.noreply.github.com>
2020-10-13 16:44:42 +0700

    nguyen

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-from flask import *
+from flask import Flask, jsonify
+
 app = Flask(__name__)
+app.config["DEBUG"] = True
 
+@app.route('/', methods=['GET'])
+def home():
+    return "HEllo"
 
-@app.route('/')
-def hello():
- return "<h1>Welcome to Geeks for Geeks</h1>"
+@app.route('/user', methods=['GET'])
+def getNextFromCurrent():
+    current_temp = 0.0
+    next_temp = 1.0
+    return jsonify({'current': current_temp, 'next': next_temp})
+
+@app.route('/iot/<float:temperature>', methods=['GET'])
+def getNext(temperature):
+    next_temp = temperature + 1.0
+    return jsonify({'next':next_temp})
+
+if __name__=='__main__':
+    app.run(threaded=True, port=5000)
+
